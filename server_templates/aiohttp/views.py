b47d434b0be3db5e965f84caef9f5e19fcc83029
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-24 12:32:55 +0300

    apply redirections

--- server_templates/aiohttp/views.py
+++ server_templates/aiohttp/views.py
@@ -3,21 +3,57 @@ from aiohttp import web
 
 async def response__index(request):
     msg = """
-response__index<br />
-<a href="/start/">START</a><br />
-<a href="/stop/">STOP</a><br />
-"""
+<!doctype html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <title>INDEX</title>
+    <meta http-equiv="refresh" content="2; url=/" />
+  </head>
+  <body>
+    response__index<br />
+    <a href="/start">START</a><br />
+    <a href="/stop">STOP</a><br />
+  </body>
+</html>
+    """
     print(msg)
-    return web.Response(body=msg)
+    return web.Response(text=msg, content_type='text/html')
 
 
 async def response__start(request):
-    msg = 'response__start'
+    msg = """
+<!doctype html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <title>INDEX</title>
+    <meta http-equiv="refresh" content="1; url=/" />
+  </head>
+  <body>
+    response__start<br />
+    <p><a href="/">Redirect</a></p>
+  </body>
+</html>
+    """
     print(msg)
-    return web.Response(text=msg)
+    return web.Response(text=msg, content_type='text/html')
 
 
 async def response__stop(request):
-    msg = 'response__stop'
+    msg = """
+    <!doctype html>
+    <html lang="en-US">
+      <head>
+        <meta charset="utf-8" />
+        <title>INDEX</title>
+        <meta http-equiv="refresh" content="1; url=/" />
+      </head>
+      <body>
+        response__stop<br />
+        <p><a href="/">Redirect</a></p>
+      </body>
+    </html>
+    """
     print(msg)
-    return web.Response(text=msg)
+    return web.Response(text=msg, content_type='text/html')
