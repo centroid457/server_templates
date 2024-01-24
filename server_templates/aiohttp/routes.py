cac97b051cc4027994bf84b95250974e5467b616
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-24 11:55:11 +0300

    add response__start/+

--- server_templates/aiohttp/routes.py
+++ server_templates/aiohttp/routes.py
@@ -1,5 +1,35 @@
-from .views import response__simplest_text
+from typing import *
+import pathlib
+
+from .views import *
 
 
+# =====================================================================================================================
+PROJECT_ROOT = pathlib.Path()
+
+
+# =====================================================================================================================
+ROUTES_STATIC: Dict[str, Callable] = {
+}
+
+ROUTES_GET: Dict[str, Callable] = {
+    '/': response__index,
+    '/start': response__start,
+    '/stop': response__stop,
+}
+
+
+# =====================================================================================================================
 def setup_routes(app):
-    app.router.add_get('/', response__simplest_text)
+    for route, response in ROUTES_STATIC.items():
+        app.router.add_static(
+            route,
+            path=PROJECT_ROOT / 'static'
+            # name='static'
+        )
+
+    for route, response in ROUTES_GET.items():
+        app.router.add_get(route, response)
+
+
+# =====================================================================================================================
--- server_templates/aiohttp/views.py
+++ server_templates/aiohttp/views.py
@@ -1,7 +1,23 @@
 from aiohttp import web
 
 
-async def response__simplest_text(request):
-    msg = 'Hello response__simplest_text!'
+async def response__index(request):
+    msg = """
+response__index<br />
+<a href="/start/">START</a><br />
+<a href="/stop/">STOP</a><br />
+"""
+    print(msg)
+    return web.Response(body=msg)
+
+
+async def response__start(request):
+    msg = 'response__start'
+    print(msg)
+    return web.Response(text=msg)
+
+
+async def response__stop(request):
+    msg = 'response__stop'
     print(msg)
     return web.Response(text=msg)
