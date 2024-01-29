93f68b11fee5f835c611594224df5e79ea3fa9ec
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-29 18:54:11 +0300

    finish fix keep all pkgs_internal in setup/build pypi

--- explore.py
+++ explore.py
@@ -1,10 +1,13 @@
-# import server_templates
-# import testplans
-# thread = server_templates.ServerAiohttpBase(testplans.TpMultyDutBase()).run()
+import server_templates
+import testplans
+thread = server_templates.ServerAiohttpBase(testplans.TpMultyDutBase()).run()
 
 
-# ====================================
-from setuptools import find_packages
-
-for name in find_packages("server_templates"):
-    print(name)
+# # ====================================
+# from setuptools import find_packages
+#
+# pkgs = find_packages("server_templates")
+#
+# print(pkgs)
+# for name in pkgs:
+#     print(f"\t{name}")
--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ with open("README.md", "r") as f:
     readme = f.read()
 
 
+packages = [PROJECT.NAME_IMPORT, ]
+pkgs_internal = find_packages(where=PROJECT.NAME_IMPORT)
+for name in pkgs_internal:
+    packages.append(f"{PROJECT.NAME_IMPORT}.{name}")
+
+
 # =====================================================================================================================
 setup(
   version=PROJECT.VERSION_STR,
@@ -42,7 +48,7 @@ setup(
     "Source": f"https://github.com/centroid457/{PROJECT.NAME_IMPORT}",
   },
 
-  packages=[PROJECT.NAME_IMPORT, *find_packages(PROJECT.NAME_IMPORT)],
+  packages=packages,
   install_requires=[],
   python_requires=">=3.6"
 )
