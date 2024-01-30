3967693c276939c46c2303eaf589c9b79b0a9730
Andrei Starichenko <a.starichenko@element-t.ru>
2024-01-30 17:59:21 +0300

    zero

--- explore.py
+++ explore.py
@@ -7,6 +7,7 @@ import time
 # =====================================================================================================================
 class Tp(TpMultyDutBase):
     START_GUI = False
+    START_API = False
 
 
 tp = Tp()
