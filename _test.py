8be9fb5b113d570293324bfd8b6cf71fdd9941e1
Andrei Starichenko <a.starichenko@element-t.ru>
2024-02-15 14:30:39 +0300

    fix just renames in Test__ServerAiohttp

--- _test.py
+++ _test.py
@@ -18,7 +18,7 @@ from server_templates import *
 class Test__ServerAiohttp:
     PORT_TEST1: int = 8081
 
-    victim_1 = None
+    victim = None
     victim_2 = None
 
     @classmethod
@@ -37,33 +37,31 @@ class Test__ServerAiohttp:
         # PREPARE ==============================================
         TEST_DATA = {'test_data_key_1': 1}
 
-        class ServerAiohttp_1(ServerAiohttpBase):
+        class Victim(ServerAiohttpBase):
             PORT = self.PORT_TEST1
             test_data = None
 
-            async def response_post__test1(self, request) -> web.Response:
+            async def response_post__test_post(self, request) -> web.Response:
                 self.test_data = await request.json()
                 return web.json_response(data=TEST_DATA)
 
-            async def response_get_json__test1(self, request) -> web.Response:
+            async def response_get_json__test_get_json(self, request) -> web.Response:
                 return web.json_response(data=TEST_DATA)
 
-        self.Victim_1 = ServerAiohttp_1
-        self.victim_1 = self.Victim_1()
+        self.victim = Victim()
 
-        class ServerAiohttp_2(ServerAiohttpBase):
+        class Victim2(ServerAiohttpBase):
             PORT = self.PORT_TEST1 + 1
 
-        self.Victim_2 = ServerAiohttp_2
-        self.victim_2 = self.Victim_2()
+        self.victim_2 = Victim2()
 
         # WORK ==============================================
-        self.victim_1.start()
-        response = requests.get(url=f"http://localhost:{self.victim_1.PORT}/", timeout=1)
+        self.victim.start()
+        response = requests.get(url=f"http://localhost:{self.victim.PORT}/", timeout=1)
 
         # double start DEPandant -------------------------
         try:
-            self.victim_1.run()
+            self.victim.run()
             assert False
         except:
             pass
@@ -73,23 +71,23 @@ class Test__ServerAiohttp:
         response = requests.get(url=f"http://localhost:{self.victim_2.PORT}/", timeout=1)
 
         # test__GET_INDEX ----------------------------------
-        response = requests.get(url=f"http://localhost:{self.victim_1.PORT}/api_index", timeout=1)
-        assert f'/api_index' in self.victim_1._ROUTES["get_html"]
+        response = requests.get(url=f"http://localhost:{self.victim.PORT}/api_index", timeout=1)
+        assert f'/api_index' in self.victim._ROUTES["get_html"]
         assert f'/api_index' in response.text
 
         # test__GET_JSON ----------------------------------
-        response = requests.get(url=f"http://localhost:{self.victim_1.PORT}/test1", timeout=1)
-        assert f'/test1' in self.victim_1._ROUTES["get_json"]
-        assert f'/{self.victim_1._ROUTE_NAME_PREFIX_HTML_FOR_JSON}test1' in self.victim_1._ROUTES["get_html"]
+        response = requests.get(url=f"http://localhost:{self.victim.PORT}/test_get_json", timeout=1)
+        assert f'/test_get_json' in self.victim._ROUTES["get_json"]
+        assert f'/{self.victim._ROUTE_NAME_PREFIX_HTML_FOR_JSON}test_get_json' in self.victim._ROUTES["get_html"]
         assert response.json() == TEST_DATA
 
-        response = requests.get(url=f"http://localhost:{self.victim_1.PORT}/{self.victim_1._ROUTE_NAME_PREFIX_HTML_FOR_JSON}test1", timeout=1)
+        response = requests.get(url=f"http://localhost:{self.victim.PORT}/{self.victim._ROUTE_NAME_PREFIX_HTML_FOR_JSON}test_get_json", timeout=1)
         assert 'test_data_key_1' in response.text
 
         # test__POST ----------------------------------
-        response = requests.post(url=f"http://localhost:{self.victim_1.PORT}/test1", timeout=1, json=TEST_DATA)
-        assert f'/test1' in self.victim_1._ROUTES["post"]
-        assert self.victim_1.test_data == TEST_DATA
+        response = requests.post(url=f"http://localhost:{self.victim.PORT}/test_post", timeout=1, json=TEST_DATA)
+        assert f'/test_post' in self.victim._ROUTES["post"]
+        assert self.victim.test_data == TEST_DATA
         assert response.json() == TEST_DATA
 
 
