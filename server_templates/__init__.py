28cba9ac17b583df461c004cd20aa5a878de2d42
Andrei Starichenko <centroid@mail.ru>
2024-04-17 18:04:44 +0300

    add fastapi into __init__.py

--- server_templates/__init__.py
+++ server_templates/__init__.py
@@ -1,3 +1,4 @@
 # from server_templates.server_aiohttp.main import *
 from server_templates.server_aiohttp import *
+from server_templates.server_fastapi import *
 from server_templates.client_requests import *
