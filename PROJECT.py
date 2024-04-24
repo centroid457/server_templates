3660ff10ce4610960d1f47e511f284a591d6f5f5
Andrei Starichenko <centroid@mail.ru>
2024-04-24 15:41:09 +0300

    ready v0.2.14

--- PROJECT.py
+++ PROJECT.py
@@ -40,7 +40,7 @@ class PROJECT:
     ]
 
     # HISTORY -----------------------------------------------
-    VERSION: Tuple[int, int, int] = (0, 2, 13)
+    VERSION: Tuple[int, int, int] = (0, 2, 14)
     TODO: List[str] = [
         "add full logger for requests (with time+body) save in file",
     ]
@@ -48,7 +48,7 @@ class PROJECT:
         "..."
     ]
     NEWS: List[str] = [
-        "[URL] separate url + tests",
+        "[test server_aiohttp] add skip ReqCheckStr_Os.bool_if__LINUX()",
     ]
 
     # FINALIZE -----------------------------------------------
